0ba8fd2ba7b51e7a135ef59b0a4315afc48ac142
Superuser <Superuser@DESKTOP-DI5B4PO.local>
2021-07-03 12:35:39 +0200

    Rename ValueGrouping to Grouping

--- src/Extensions/GroupingExtensions.cs
+++ src/Extensions/GroupingExtensions.cs
@@ -10,7 +10,7 @@ namespace KeyValueCollection.Extensions
     public static class GroupingExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ReadOnlyGrouping<TKey, TElement> ToImmutable<TKey, TElement>(this ValueGrouping<TKey, TElement> grouping)
+        public static ReadOnlyGrouping<TKey, TElement> ToImmutable<TKey, TElement>(this Grouping<TKey, TElement> grouping)
             where TKey : notnull
         {
             return new(grouping.Elements, 0, grouping.Count, grouping.Key, grouping.HashCode);
